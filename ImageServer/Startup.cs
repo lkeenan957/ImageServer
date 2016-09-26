475717693326bf0c45c10156d565d36e73af4c47
Lydia Keenan <LYDIAK@our.ecu.edu.au>
2016-09-26 08:25:28 -0700

    v1

---
+++ ImageServer/Startup.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.Owin;
+using Owin;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using ImageServer.Models;
+
+[assembly: OwinStartupAttribute(typeof(ImageServer.Startup))]
+namespace ImageServer
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+            CreateRoles();
+        }
+
+        public void CreateRoles()
+        {
+            var roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>());
+            var UserManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>());
+
+            // Check for Provider users.  If not exists we created it.
+            if(!roleManager.RoleExists("Provider"))
+            {
+                var role = new IdentityRole();
+                role.Name = "Provider";
+                roleManager.Create(role);
+           
+            }
+
+            else if(!roleManager.RoleExists("Administrator"))
+            {
+                var role = new IdentityRole();
+                role.Name = "Administrator";
+                roleManager.Create(role);
+            }
+
+            else(!roleManager.RoleExists("Moderator"))
+            {
+                var role = new IdentityRole();
+                role.Name = "Moderator";
+                roleManager.Create(role);
+            }
+
+            else(!roleManger.RoleExists("Consumer"))
+            {
+                var role = new IdentityRole();
+                role.Name = "Consumer";
+                roleManager.Create(role);
+            }
+        }
+    }
+}
